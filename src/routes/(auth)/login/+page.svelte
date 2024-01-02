1f79a848d5b2b6289b1034c0dd2573de1e76487d
bruno <bruno.resende@gmx.com>
2024-01-02 09:16:02 -0300

    Feat: Basic Login Flow Added

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -8,22 +8,6 @@
 	import { GithubIcon } from 'lucide-svelte';
 </script>
 
-<p>
-	{#if $page.data.session}
-		{#if $page.data.session.user?.image}
-			<img src={$page.data.session.user.image} alt="avatar" class="w-24 rounded-full" />
-		{/if}
-		<span class="signedInText">
-			<small>Signed in as</small><br />
-			<strong>{$page.data.session.user?.name ?? 'User'}</strong>
-		</span>
-		<button on:click={() => signOut()} class="button">Sign out</button>
-	{:else}
-		<span class="notSignedInText">You are not signed in</span>
-		<button on:click={() => signIn('github')}>Sign In with GitHub</button>
-	{/if}
-</p>
-
 <div class="mt-24 rounded bg-black/80 p-6 py-10 md:mt-0 md:max-w-sm md:px-14">
 	<form method="POST" use:enhance>
 		<h1 class="text-3xl font-semibold text-white">Entrar</h1>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
+	import { page } from '$app/stores';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { signOut } from '@auth/sveltekit/client';
 </script>
 
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
-
-<Button variant="default">Button</Button>
+<div
+	class="mt-24 flex flex-col justify-center rounded bg-black/80 p-6 py-10 md:mt-0 md:max-w-sm md:px-14"
+>
+	{#if $page.data.session}
+		{#if $page.data.session.user?.image}
+			<img src={$page.data.session.user.image} alt="avatar" class="w-24 rounded-full" />
+		{/if}
+		<span class="signedInText">
+			<small>Signed in as</small><br />
+			<strong>{$page.data.session.user?.name ?? 'User'}</strong>
+		</span>
+		<Button on:click={() => signOut()} class="button">Sign out</Button>
+	{/if}
+</div>
